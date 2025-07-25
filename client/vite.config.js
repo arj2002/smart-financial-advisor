28e701b0a39407bda7ab11fc68a0d52cf15147e4
Anjali Raj <91485159+arj2002@users.noreply.github.com>
2025-07-25 15:10:31 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+  },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      external: [], // we’ll update this when the problematic module is known
+    },
+  },
 })
